3793c81eca318db2f87d75e959d3988c88410790
Sergey Lukashyn <lsoft.riverside@gmail.com>
2018-12-14 18:24:53 +0200

    синтаксическая ошибка

--- HomeWorkLinq/Program.cs
+++ HomeWorkLinq/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 
+// https://github.com/LSoftRiversige/Homework-13-dec-2018.git
+
 namespace HomeWorkLinq
 {
     class Program
@@ -8,12 +10,12 @@ namespace HomeWorkLinq
         static void Main(string[] args)
         {
 
-            //LinqLS.FloatToInt(new float[] { 1.14f, 2.99f, 33, 44, 555, 0 }).DisplayToConsole();
+            LinqLS.FloatToInt(new float[] { 1.14f, 2.99f, 33, 44, 555, 0 }).DisplayToConsole();
 
-            //LinqLS.NumsToBool(new int[] { 1, 2, 3, 4, 5, -1 }).DisplayToConsole();
+            LinqLS.NumsToBool(new int[] { 1, 2, 3, 4, 5, -1 }).DisplayToConsole();
 
-            //IEnumerable<string> vovelless = LinqLS.ExtractVowels(new List<string> { "привет", "одесса", "мир", "информатика"});
-            //vovelless.DisplayToConsole();
+            IEnumerable<string> vowelless = LinqLS.ExtractVowels(new List<string> { "привет", "одесса", "мир", "информатика"});
+            vowelless.DisplayToConsole();
 
 
 
